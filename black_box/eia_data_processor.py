106db9ce8af361c201bafab83ded3b3e39c86666
Angel Salazar <aangelsalazarr@gmail.com>
2022-10-29 15:25:45 -0400

    re set up

--- black_box/eia_data_processor.py
+++ black_box/eia_data_processor.py
@@ -3,6 +3,8 @@ import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import rc
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 # setting up params
 rc('mathtext', default='regular')
@@ -22,12 +24,64 @@ def is_float(x):
 
 
 # purpose is to grab data and transform it
-def grab_eia_data(url):
+def sun_energy_gen_data(state, start_date):
+    '''
+
+    :param state:
+    :param start_date:
+    :return:
+    '''
+
+    # base url
+    baseUrl = 'https://api.eia.gov/v2/electricity/facility-fuel/data/'
+    baseUrl = baseUrl + '?api_key=' + str(api_key)
+
+    # we will want to add some inputs to our complete url
+    urlData = '&data[0]=generation'
+    urlFreq = '&frequency=monthly'
+    urlStart = '&start=' + str(start_date)
+    urlFacet = '&facets[fuel2002][0]=SUN&facets[state][0]=' + str(state)
+
+    # concatenate to get main url
+    url = baseUrl + urlData + urlFacet + urlFreq + urlStart
+
     r = requests.get(url=url)  # store url request as variable r
     data = r.json()  # convert our request into json format
     entries = data['response']['data']  # only grabbing data aspect of return
-    eia_df = pd.DataFrame(data=entries)  # converting our data into a pandas df
-    eia_df.reset_index(drop=True)  # resetting df index
-    eia_df['period'] = pd.to_datetime(eia_df['period'])  # convert period type
+    df = pd.DataFrame(data=entries)  # converting our data into a pandas df
+    df.reset_index(drop=True)  # resetting df index
+    # df['period'] = pd.to_datetime(df['period'])  # convert period type
 
-    return eia_df  # return our now transformed df
+    return df  # return our now transformed df
+
+
+def wind_energy_gen_data(state, start_date):
+    '''
+
+    :param state:
+    :param start_date:
+    :return:
+    '''
+
+    # base url
+    baseUrl = 'https://api.eia.gov/v2/electricity/facility-fuel/data/'
+    baseUrl = baseUrl + '?api_key=' + str(api_key)
+
+    # we will want to add some inputs to our complete url
+    urlData = '&data[0]=generation'
+    urlFreq = '&frequency=monthly'
+    urlStart = '&start=' + str(start_date)
+    urlFacet = '&facets[fuel2002][0]=WND&facets[state][0]=' + str(state)
+
+    # concatenate to get main url
+    url = baseUrl + urlData + urlFacet + urlFreq + urlStart
+
+    r = requests.get(url=url)  # store url request as variable r
+    data = r.json()  # convert our request into json format
+    entries = data['response']['data']  # only grabbing data aspect of return
+    df = pd.DataFrame(data=entries)  # converting our data into a pandas df
+    df.reset_index(drop=True)  # resetting df index
+    # df['period'] = pd.to_datetime(df['period'])  # convert period type
+
+    return df  # return our now transformed df
+
--- eia_solar_generation.py
+++ eia_solar_generation.py
@@ -7,11 +7,9 @@ from matplotlib import rc
 import seaborn as sns
 pd.set_option('display.max_columns', None)
 
-
 # some params related to the framework of pdf output that we will need
 rc('mathtext', default='regular')
 plt.rcParams["figure.autolayout"] = True
-
 
 # grabbing our api key
 api_key = os.environ.get('eiaAPI')
@@ -19,28 +17,12 @@ api_key = os.environ.get('eiaAPI')
 # setting up some date variables to use
 today = date.today()
 currentDate = today.strftime('%m_%d_%y')
-startDate = today - relativedelta(years=5)
+startDate = today - relativedelta(years=7)
 eiaStartDate = startDate.strftime('%Y-%m')
 
-
-
-# base url
-baseUrl = 'https://api.eia.gov/v2/electricity/facility-fuel/data/'
-baseUrl = baseUrl + '?api_key=' + str(api_key)
-
-# we will want to add some inputs to our complete url
-urlData = '&data[0]=generation&data[1]=gross-generation'
-urlFreq = '&frequency=monthly'
-urlStart = '&start=' + str(eiaStartDate)
-urlFacet = '&facets[fuel2002][0]=SUN&facets[fuel2002][1]=WND&facets[state][' \
-           '0]=CA'
-
-# concatenate to get main url
-mainUrl = baseUrl + urlData + urlFacet + urlFreq + urlStart
-print(mainUrl)
-
 # storing our get request as a df variable
-df = grab_eia_data(url=mainUrl)
+df = sun_energy_gen_data(state='CA', start_date=eiaStartDate)
+print(df)
 
 # convert our new df data into csv
-df.to_csv('./data_files/ca_solar_wind_gen_data.csv', index=False)
+# df.to_csv('./data_files/ca_solar_wind_gen_data.csv', index=False)
