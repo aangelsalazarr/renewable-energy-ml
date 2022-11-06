abed7c49f5502ca4bba16e26ce31ced138cb90e2
Angel Salazar <aangelsalazarr@gmail.com>
2022-11-06 15:12:33 -0500

    created function for NCEI to grab weather data

---
+++ black_box/ncei_data_processor.py
@@ -0,0 +1,44 @@
+import os
+import requests
+import pandas as pd
+from datetime import date
+from dateutil.relativedelta import relativedelta
+import matplotlib.pyplot as plt
+
+# grabbing our api key if necessary
+apiKey = os.environ.get('nceiKey')
+
+
+def grab_ncei_data(dataType, stations, start, end):
+
+    '''
+    :param dataType:
+    :param stations:
+    :param start:
+    :param end:
+    :return:
+    '''
+
+    # predefined url that will be used to request data
+    baseUrl = 'https://www.ncei.noaa.gov/access/services/data/v1/?'
+
+    # additional elements of the url
+    dataSet = 'dataset=' + 'daily-summaries'
+    dataType = '&dataTypes=' + str(dataType)  # 'AWND,WSF2,WS5'
+    stations = '&stations=' + str(stations)  # 'USW00094846,USW00014925'
+    startDate = '&startDate=' + str(start)  # '2015-01-01'  # YYYY-MM-DD
+    endDate = '&endDate=' + str(end)  # '2020-01-01'
+    units = '&units=' + 'standard'
+    set_format = '&format=' + 'json'
+
+    # this will be the main url that will be used in our GET request
+    url = baseUrl + dataSet + dataType + stations + startDate + endDate + \
+          units + set_format
+
+    # process the url data
+    r = requests.get(url=url)
+    data = r.json()
+    df = pd.DataFrame(data=data)
+
+    return df
+
---
+++ ncei_data.py
@@ -0,0 +1,20 @@
+from black_box.ncei_data_processor import grab_ncei_data
+
+# grabbing temperature data for texas by setting params first
+
+# temp average, min, max, avg wind speed, precipitation and weather type
+data_type = 'TAVG,TMAX,TMIN,AWND,PRCP'
+
+# houston william hobby airport and houston intercontinental airport
+htx_stations = 'USW00012918,USW00012960'
+
+# start and end dates of data set in YYYY-MM-DD
+start = '2019-01-01'
+end = '2020-01-01'
+
+# using our function to grab data
+htx_df = grab_ncei_data(dataType=data_type, stations=htx_stations,
+                          start=start, end=end)
+
+# converting out data into csv output for observation
+htx_df.to_csv('./data_files/htx_weather_data_2019_to_2020.csv', index=False)
